ec18c9d2531d8322f750da825b33b1706060164a
anirudhAgniRedhat <aagnihot@redhat.com>
2024-04-29 14:17:51 +0530

    trying *string

--- machine/v1beta1/zz_generated.deepcopy.go
+++ machine/v1beta1/zz_generated.deepcopy.go
@@ -314,6 +314,11 @@ func (in *AzureMachineProviderSpec) DeepCopyInto(out *AzureMachineProviderSpec) 
 		(*in).DeepCopyInto(*out)
 	}
 	in.Diagnostics.DeepCopyInto(&out.Diagnostics)
+	if in.CapacityReservationGroupID != nil {
+		in, out := &in.CapacityReservationGroupID, &out.CapacityReservationGroupID
+		*out = new(string)
+		**out = **in
+	}
 	return
 }
 
